c6b9af4802ba71daf26893e3a73ebfb74b132d58
GauthamAskani <89175644+GauthamAskani@users.noreply.github.com>
2024-12-15 22:15:26 +0000

    order.py

--- order.py
+++ order.py
@@ -1,13 +1,13 @@
 from mongoengine import Document, ReferenceField, StringField, ListField
 from app.models.schemas import OrderSchema
-from app.models.address import Address  # Import the Address model
+from app.models.address import Address  
 
 class Order(Document):
     user_id = ReferenceField('User', required=True)
     products = ListField(ReferenceField('Product'), required=True)
     order_id = StringField(required=True, unique=True)
-    status = StringField(required=True)  # e.g., "pending", "completed", etc.
-    address = ReferenceField(Address, required=True)  # Link to Address model
+    status = StringField(required=True)  
+    address = ReferenceField(Address, required=True)  
 
     def serialize(self):
         order_schema = OrderSchema()
